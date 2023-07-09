f7b1c80a3f7179369a835ef36989d098be9e365f
mfreeman451 <mfreeman451@gmail.com>
2023-07-08 21:14:46 -0500

    💄 added more races, classes, and alignment

--- web/app.vue
+++ web/app.vue
@@ -19,6 +19,66 @@
           <option value="Human">Human</option>
           <option value="Elf">Elf</option>
           <option value="Dwarf">Dwarf</option>
+          <option value="Halfling">Halfling</option>
+          <option value="Gnome">Gnome</option>
+          <option value="Half-Elf">Half-Elf</option>
+          <option value="Half-Orc">Half-Orc</option>
+          <option value="Dragonborn">Dragonborn</option>
+          <option value="Tiefling">Tiefling</option>
+          <option value="Aarakocra">Aarakocra</option>
+          <option value="Genasi">Genasi</option>
+          <option value="Goliath">Goliath</option>
+          <option value="Aasimar">Aasimar</option>
+          <option value="Bugbear">Bugbear</option>
+          <option value="Firbolg">Firbolg</option>
+          <option value="Goblin">Goblin</option>
+          <option value="Hobgoblin">Hobgoblin</option>
+          <option value="Kenku">Kenku</option>
+          <option value="Kobold">Kobold</option>
+          <option value="Lizardfolk">Lizardfolk</option>
+          <option value="Orc">Orc</option>
+          <option value="Tabaxi">Tabaxi</option>
+          <option value="Triton">Triton</option>
+          <option value="Yuan-Ti Pureblood">Yuan-Ti Pureblood</option>
+          <option value="Tortle">Tortle</option>
+          <option value="Gith">Gith</option>
+          <option value="Changeling">Changeling</option>
+          <option value="Kalashtar">Kalashtar</option>
+          <option value="Shifter">Shifter</option>
+          <option value="Warforged">Warforged</option>
+          <option value="Centaur">Centaur</option>
+          <option value="Loxodon">Loxodon</option>
+          <option value="Minotaur">Minotaur</option>
+          <option value="Simic Hybrid">Simic Hybrid</option>
+          <option value="Vedalken">Vedalken</option>
+          <option value="Verdan">Verdan</option>
+          <option value="Locathah">Locathah</option>
+          <option value="Grung">Grung</option>
+          <option value="Orc of Exandria">Orc of Exandria</option>
+          <option value="Hobgoblin of Exandria">Hobgoblin of Exandria</option>
+          <option value="Leonin">Leonin</option>
+          <option value="Satyr">Satyr</option>
+          <option value="Aetherborn">Aetherborn</option>
+          <option value="Aven">Aven</option>
+          <option value="Khenra">Khenra</option>
+          <option value="Minotaur">Minotaur</option>
+          <option value="Naga">Naga</option>
+          <option value="Sphinx">Sphinx</option>
+          <option value="Vampire">Vampire</option>
+          <option value="Vampire Spawn">Vampire Spawn</option>
+          <option value="Viashino">Viashino</option>
+          <option value="Locathah">Locathah</option>
+          <option value="Orc of Eberron">Orc of Eberron</option>
+          <option value="Shifter">Shifter</option>
+          <option value="Warforged">Warforged</option>
+          <option value="Changeling">Changeling</option>
+          <option value="Kalashtar">Kalashtar</option>
+          <option value="Shardmind">Shardmind</option>
+          <option value="Wilden">Wilden</option>
+          <option value="Deva">Deva</option>
+          <option value="Githzerai">Githzerai</option>
+          <option value="Revenant">Revenant</option>
+          <option value="Shadar-Kai">Shadar-Kai</option>
           <!-- Add more options as needed -->
         </select>
       </div>
@@ -29,7 +89,33 @@
           <option value="Fighter">Fighter</option>
           <option value="Wizard">Wizard</option>
           <option value="Rogue">Rogue</option>
+          <option value="Cleric">Cleric</option>
+          <option value="Bard">Bard</option>
+          <option value="Barbarian">Barbarian</option>
+          <option value="Druid">Druid</option>
+          <option value="Monk">Monk</option>
+          <option value="Paladin">Paladin</option>
+          <option value="Ranger">Ranger</option>
+          <option value="Sorcerer">Sorcerer</option>
+          <option value="Warlock">Warlock</option>
+          <option value="Artificer">Artificer</option>
+          <option value="Blood Hunter">Blood Hunter</option>
           <!-- Add more options as needed -->
+        </select>
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="alignment">Alignment</label>
+        <select v-model="character.alignment" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          <option value="">Select an alignment</option>
+          <option value="Lawful Good">Lawful Good</option>
+          <option value="Neutral Good">Neutral Good</option>
+          <option value="Chaotic Good">Chaotic Good</option>
+          <option value="Lawful Neutral">Lawful Neutral</option>
+          <option value="True Neutral">True Neutral</option>
+          <option value="Chaotic Neutral">Chaotic Neutral</option>
+          <option value="Lawful Evil">Lawful Evil</option>
+          <option value="Neutral Evil">Neutral Evil</option>
+          <option value="Chaotic Evil">Chaotic Evil</option>
         </select>
       </div>
       <div class="flex items-center justify-between">
