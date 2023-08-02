49676958efd8dff4b0d78da9fdf5cadd5fd6f165
mfreeman451 <mfreeman451@gmail.com>
2023-08-02 14:00:25 -0500

    ✨ crqs implementation roughed in

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"github.com/joho/godotenv"
 	"github.com/mfreeman451/dd-chatgpt-dm/client/pb/game"
@@ -12,6 +13,20 @@ import (
 )
 
 func main() {
+	// Define flags
+	createUserFlag := flag.Bool("create-user", false, "Create a new user")
+	listUsersFlag := flag.Bool("list-users", false, "List all users")
+	createGameFlag := flag.Bool("create-game", false, "Create a new game")
+	flag.Parse()
+
+	// Check if any flags were provided
+	if !*createUserFlag && !*createGameFlag {
+		fmt.Println("Usage: main [-create-user] [-create-game]")
+		fmt.Println("  -create-user: Create a new user")
+		fmt.Println("  -create-game: Create a new game")
+		os.Exit(0)
+	}
+
 	// load .env
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("failed to load .env: %v", err)
@@ -31,28 +46,44 @@ func main() {
 
 	client := game.NewGameClient(conn)
 
-	// Create player
-	req := &game.CreatePlayerRequest{
-		Player: &game.Player{
-			Name: "Test Player",
-		},
-	}
-	resp, err := client.CreatePlayer(context.Background(), req)
+	if *createUserFlag {
+		// Create player
+		req := &game.CreatePlayerRequest{
+			Player: &game.Player{
+				Name: "Test Player",
+			},
+		}
+		resp, err := client.CreatePlayer(context.Background(), req)
 
-	if err != nil {
-		log.Fatalf("failed to create player: %v", err)
-	}
+		if err != nil {
+			log.Fatalf("failed to create player: %v", err)
+		}
 
-	log.Printf("Created player: %v", resp.Player)
-
-	// List Players
-	req2 := &game.ListPlayersRequest{}
-	resp2, err := client.ListPlayers(context.Background(), req2)
-	if err != nil {
-		log.Fatalf("failed to list players: %v", err)
+		log.Printf("Created player: %v", resp.Player)
 	}
 
-	for _, player := range resp2.Players {
-		log.Printf("Player: %v\n", player)
+	if *listUsersFlag {
+		// List Players
+		req2 := &game.ListPlayersRequest{}
+		resp2, err := client.ListPlayers(context.Background(), req2)
+		if err != nil {
+			log.Fatalf("failed to list players: %v", err)
+		}
+
+		for _, player := range resp2.Players {
+			log.Printf("Player: %v\n", player)
+		}
+	}
+
+	if *createGameFlag {
+		createGame, err := client.CreateGame(context.Background(), &game.CreateGameCommand{
+			GameId:   "dnd",
+			PlayerId: "test-player",
+		})
+		if err != nil {
+			log.Fatalf("failed to create game: %v", err)
+		}
+
+		fmt.Println("Game created: ", createGame.GameId)
 	}
 }
