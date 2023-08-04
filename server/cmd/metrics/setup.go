43372b73c4a65c86b2cff353d33d60616859119e
mfreeman451 <mfreeman451@gmail.com>
2023-08-04 10:20:32 -0500

    ♻️ cleanup metrics setup.go

--- server/cmd/metrics/setup.go
+++ server/cmd/metrics/setup.go
@@ -49,33 +49,3 @@ func (m *MetricsService) Stop() {
 		_ = m.server.Close()
 	}
 }
-
-// SetupMetricsServer sets up the Prometheus metrics server.
-func SetupMetricsServer(log logger.Logger) error {
-	// Register a counter metric to track incoming requests.
-	requestsCounter := prometheus.NewCounter(prometheus.CounterOpts{
-		Name: "dnd_requests_total",
-		Help: "Total number of incoming requests",
-	})
-	prometheus.MustRegister(requestsCounter)
-
-	// Expose Prometheus metrics via HTTP endpoint
-	http.Handle("/metrics", promhttp.Handler())
-
-	// Start HTTP server for Prometheus metrics
-	go func() {
-		log.Info().Msg("Starting Prometheus Metrics server")
-		httpPort, err := strconv.Atoi(os.Getenv("PROMETHEUS_PORT"))
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to convert PROMETHEUS_PORT to int")
-			return
-		}
-		httpAddr := fmt.Sprintf(":%d", httpPort)
-		log.Info().Msgf("Prometheus metrics server listening on %s", httpAddr)
-		if err := http.ListenAndServe(httpAddr, nil); err != nil {
-			log.Fatal().Err(err).Msg("failed to start Prometheus metrics server")
-			return
-		}
-	}()
-	return nil
-}
